81a372573bd636422f01ba8b93e64703fd9afcc3
CyberGodSA <danil.gartvig@gmail.com>
2017-05-27 17:38:05 +0300

    second commit

--- client.py
+++ client.py
@@ -5,22 +5,37 @@ import sys
 from tkinter import *
 
 f = open(r'C:\Users\Danil\PycharmProjects\lab_socket_editor\client_history.txt', 'a')
-f.write('---------------------------------------------------------------------\n')
+f.write('###########new connection##########\n')
 host = ''
 header = 'H'
 
 filename = None
 
 
+def write_history(s):
+    global f
+    f.write(time.asctime(time.localtime(time.time())) + '\t' + s + '\n')
+    f.close()
+
+
 def get_host():
     global host
-    print('Input server ip adress: ')
+    print('Input server ip address: ')
     host = input()
+    write_history('connection to: ' + host)
 
 
 def send_text():
     data = textPad.get('1.0', END)
     sock.send(bytes(data, encoding='utf-8'))
+    write_history('server_send: ' + data)
+
+
+def quit_text():
+    write_history('end of connection')
+    root.quit()
+    f.close()
+    sock.close()
 
 
 # main
@@ -34,7 +49,9 @@ sock.connect((host, 1035))
 
 while True:
     text__ = sock.recv(1024).decode('utf-8')
-    if not text__: break
+    print(text__)
+    if not text__:
+        break
     root = Tk()
     root.title("Client Text Editor")
     textPad = Text(root, width=100, height=80)
@@ -44,8 +61,7 @@ while True:
     menu = Menu(root)
     root.config(menu=menu)
     menu.add_command(label="Send", command=send_text)
-
-    textPad.bind("<KeyPress>", send_text)
+    menu.add_command(label="Quit", command=quit_text)
 
     textPad.pack()
     root.mainloop()
--- server.py
+++ server.py
@@ -1,12 +1,20 @@
 import socket
 import time
 
-hypertext = r'C:\Users\Danil\PycharmProjects\lab_socket_editor\hypertext.txt'
-tcp = r'C:\Users\Danil\PycharmProjects\lab_socket_editor\tcp.txt'
-www = r'C:\Users\Danil\PycharmProjects\lab_socket_editor\www.txt'
 history = r'C:\Users\Danil\PycharmProjects\lab_socket_editor\server_history.txt'
-header = 'H'
 text = open(r'C:\Users\Danil\PycharmProjects\lab_socket_editor\text.txt', 'r')
+header = "H"
+
+
+def who(conn):
+    cmd = 'info '
+    author = 'The author: Hartwig Danil'
+    task = 'The task: 1. Socket Text Editor'
+    conn.send(bytes(header + cmd + author, encoding='utf-8'))
+    write_history('server_send: ' + author)
+    time.sleep(0.1)
+    conn.send(bytes(header + cmd + task, encoding='utf-8'))
+    write_history('server_send: ' + task)
 
 
 def write_history(s):
@@ -22,8 +30,18 @@ sock.bind(('', 1035))
 sock.listen(1)
 conn, addr = sock.accept()
 print('connected:', addr)
+write_history('connected to: ' + addr)
 conn.send(bytes(text.read(), encoding='utf-8'))
 
 while True:
     text = conn.recv(1024).decode('utf-8')
-    print(text)
+    if text == "who":
+        who(conn)
+    elif text == "end":
+        break
+    else:
+        print(text)
+
+    write_history('server_get: ' + text)
+
+conn.close()
